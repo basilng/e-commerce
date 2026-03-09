6bd4d193b229d0df199164ec8e065a510b8db8b3
Basil N G <basilng.bng@gmail.com>
2026-03-09 14:58:28 +0530

    Distributed tracing added

--- order/src/main/java/com/bng/ecommerce/order/client/RestClientConfig.java
+++ order/src/main/java/com/bng/ecommerce/order/client/RestClientConfig.java
@@ -1,10 +1,15 @@
 package com.bng.ecommerce.order.client;
 
+import io.micrometer.observation.ObservationRegistry;
+import io.micrometer.tracing.Tracer;
+import io.micrometer.tracing.propagation.Propagator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.http.HttpStatusCode;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
@@ -19,16 +24,45 @@ import java.util.Optional;
 @Configuration
 public class RestClientConfig {
 
+    @Autowired(required = false)
+    private ObservationRegistry observationRegistry;
+
+    @Autowired(required = false)
+    private Tracer tracer;
+
+    @Autowired(required = false)
+    private Propagator propagator;
+
     @Bean
     @Primary
     public RestClient.Builder restClientBuilder() {
-        return RestClient.builder();
+        RestClient.Builder builder = RestClient.builder();
+
+        if (observationRegistry != null) {
+            builder.requestInterceptor(createTracingInterceptor());
+        }
+        return builder;
+    }
+
+    private ClientHttpRequestInterceptor createTracingInterceptor() {
+        return ((request, body, execution) -> {
+            if (tracer != null && propagator != null && tracer.currentSpan() != null) {
+                propagator.inject(tracer.currentTraceContext().context(), request.getHeaders(),
+                        (carrier, key, value) -> carrier.add(key, value));
+            }
+            return execution.execute(request, body);
+        });
     }
 
     @Bean
     @LoadBalanced
     public RestClient.Builder loadBalancedRestClientBuilder() {
-        return RestClient.builder();
+        RestClient.Builder builder = RestClient.builder();
+
+        if (observationRegistry != null) {
+            builder.requestInterceptor(createTracingInterceptor());
+        }
+        return builder;
     }
 
     @Bean
@@ -53,4 +87,6 @@ public class RestClientConfig {
         HttpServiceProxyFactory factory = HttpServiceProxyFactory.builderFor(adapter).build();
         return factory.createClient(UserHttpInterface.class);
     }
+
+
 }
